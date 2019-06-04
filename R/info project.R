5d26fbe395e6d447eb468d607ab669ad6fea6d26
prettytotoro <13237158593@163.com>
2019-06-04 16:53:22 -0700

    version 4.0

--- R/info project.R
+++ R/info project.R
@@ -5,6 +5,7 @@ library("dplyr")
 library("tidyr")
 library("ggplot2")
 library("lubridate")
+library(plotly)
 
 # Get data through NYT API
 begin_date <- "20170805"
@@ -30,30 +31,44 @@ nyt_data <- rbind_pages(pages)
 
 # Make plot function
 
+my_color <- c(
+  "rgb(241, 188, 172)", "rgb(241, 171, 206)", "rgb(223, 172, 240)",
+  "rgb(170, 172, 240)", "rgb(128, 162, 227)", "rgb(167, 129, 226)",
+  "rgb(227, 129, 211)", "rgb(227, 127, 137)"
+)
+
 first_graph <- function(date, name) {
   if (name == "Twitter") {
     twitter <- read.csv("data/tweet_count_time_series.csv", stringsAsFactors = FALSE)
     twitter_data <- twitter %>% group_by(created_at_day, created_at_hour) %>% summarise(sum_count = sum(tweet_count)) %>%
       filter(created_at_day %in% date)
-    ggplot(data = twitter_data, aes(x = created_at_hour, y = sum_count, group = created_at_day, colour = as.factor(created_at_day)))+
-    geom_line()
+    plot_ly(twitter_data, x = ~created_at_hour, y = ~sum_count, name = 'High 2014', type = 'scatter', mode = 'lines', 
+            group = twitter_data$created_at_day, line = list(color = my_color[0:length(date)], width = 4))
   } else {
     NYT <- nyt_data %>% mutate(day = day(response.docs.pub_date)) %>% select(day) %>% 
       group_by(day) %>% count(day) %>% filter(day %in% date)
     
-    ggplot(data = NYT, aes(x = day, y = n, fill = day)) +
-        geom_bar(stat = "identity") +
-        theme_classic() +
-        labs(
-          x = "The date article published",
-          y = "The number of article published",
-          title = paste(
-            "New York Time paper article"
-        )
-      )
+    plot_ly(NYT,
+                 x = ~day,
+                 y = ~n,
+                 type = "bar",
+                 marker = list(color = my_color[0:length(date)]),
+                 hoverinfo = "text",
+                 text = ~ paste0(
+                   "Date:", "8/", day,
+                   "<br>Number of Atricle:", n
+                 )
+    ) %>%
+      layout(
+        title = "New York Time",
+        font = list(color = "#C0C0C0"),
+        xaxis = list(title = "The date atricle published"),
+        yaxis = list(title = "The number of article published"),
+        paper_bgcolor = "transparent",
+        plot_bgcolor = "transparent"
+    )
   }
 }
-
 
 
 
