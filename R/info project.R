2b7ff8b91037578f74bac1acfecdc81c93f3072c
dr35-1623329 <35281404+dr35-1623329@users.noreply.github.com>
2019-06-04 16:34:49 -0700

    pushing changes

--- R/info project.R
+++ R/info project.R
@@ -1,20 +1,21 @@
-##install.packages("devtools", "jsonlite")
+install.packages("devtools")
+install.packages("tidyr")
 library("jsonlite")
 library("devtools")
 library("dplyr")
 library("tidyr")
 library("ggplot2")
 
-twitter <- read.csv("data/tweet_count_time_series.csv", stringsAsFactors = FALSE)
+twitter <- read.csv("C:/Junior Year/INFO/blue-cat-33/data/tweet_count_time_series.csv", stringsAsFactors = FALSE)
 
 begin_date <- "20170805"
 end_date <- "20170825"
 term <- "Charlottesville+Virginia"
-source("desktop/key/key.R")
+nyt_key <- "z8ha000iwkU3s7jPUSsH1wG2LfaGVSoZ"
 
 nyt_url <- paste0("http://api.nytimes.com/svc/search/v2/articlesearch.json?q=",term,
                   "&begin_date=",begin_date,"&end_date=",end_date,
-                  "&facet_filter=true&api-key=",key, sep="")
+                  "&facet_filter=true&api-key=",nyt_key, sep="")
 
 initialQuery <- fromJSON(nyt_url)
 maxPages <- round((initialQuery$response$meta$hits[1] / 10)-1) 
@@ -28,9 +29,29 @@ for(i in 0:maxPages){
 
 nyt_data <- rbind_pages(pages)
 
-date <- c("16", "17", "18", "19")
-date_19 <- twitter %>% filter(created_at_day == "19")
-for (i in length(date)) {
-  
+View(nyt_data)
+
+first_graph <- function(date, name) {
+  if (name == "Twitter") {
+    twitter <- read.csv("data/tweet_count_time_series.csv", stringsAsFactors = FALSE)
+    twitter_data <- twitter %>% group_by(created_at_day, created_at_hour) %>% summarise(sum_count = sum(tweet_count)) %>%
+      filter(created_at_day %in% date)
+    ggplot(data = twitter_data, aes(x = created_at_hour, y = sum_count, group = created_at_day, colour = as.factor(created_at_day)))+
+      geom_line()
+  } else {
+    NYT <- nyt_data %>% mutate(day = day(response.docs.pub_date)) %>% select(day) %>% 
+      group_by(day) %>% count(day) %>% filter(day %in% date)
+    
+    ggplot(data = NYT, aes(x = day, y = n, fill = day)) +
+      geom_bar(stat = "identity") +
+      theme_classic() +
+      labs(
+        x = "The date article published",
+        y = "The number of article published",
+        title = paste(
+          "New York Time paper article"
+        )
+      )
+  }
 }
 
