981dad545fb232501d7774c6ae760873ae4a63a5
MAX Beeson <mbeeson7@uw.edu>
2019-06-04 20:58:32 -0700

    nyt csv

--- words_plotting.R
+++ words_plotting.R
@@ -6,6 +6,7 @@ library("ggplot2")
 library("tidyr")
 library("plotly")
 library("jsonlite")
+library("lubridate")
 data(stop_words)
 
 
@@ -16,6 +17,11 @@ if(data_source == "Twitter")
   dfs <- get_twitter_df(day)
   twitter_bar_plot(dfs, day, pos_or_neg)
   pct_graph_twitter(dfs, day)
+}else(data_source == "New York Times")
+  {
+    df <- nyt_data_as_df(day)
+    nyt_bar_plot(df, day, pos_or_neg)
+    pct_graph_nyt(df, day)
 }
 }
 
@@ -76,6 +82,7 @@ else if(pos_or_neg == "Negative")
 }
 else if(pos_or_neg == "Positive and Negative")
 {
+  twitter_token_df <- twitter_token_df
   twitter_ggtitle <- ggtitle(paste("Top 20 Most used Words in Charlottesville Tweets on August",
                                    day))
 }
@@ -87,7 +94,7 @@ twitter_plot <- ggplot(data = top_twenty_words_twitter,aes(x = word, y = n)) +
   geom_col(stat = "indentity")
 
 
-twitter_plot + twitter_ggtheme + twitter_ggtitle
+return(twitter_plot + twitter_ggtheme + twitter_ggtitle)
 }
 
 pct_graph_twitter<- function(data_frame_twitter, day)
@@ -105,5 +112,69 @@ p <- plot_ly(twitter_summary_pct, labels = ~sentiment, values = ~s_sum, type = '
 p
 }
 
-nyt_data <- read.csv("data/nyt_data.csv", stringsAsFactors = FALSE)
+nyt_data_as_df <- function(day)
+{
+nyt_data <- read.csv("data/nyt_data.csv", stringsAsFactors = F)
+nyt_data <- mutate(nyt_data, response.docs.pub_date = day(response.docs.pub_date))
+nyt_data <- mutate(nyt_data, response.docs.snippet = 
+                     paste(response.docs.lead_paragraph, response.docs.abstract, 
+                           response.docs.snippet))
+nyt_data <- filter(nyt_data, response.docs.pub_date == day)
+
+sentiment <- get_sentiments("bing")
+nyt_token_df <- unnest_tokens(nyt_data, word, response.docs.snippet)
+
+nyt_token_df <- nyt_token_df %>%
+  inner_join(sentiment) %>%
+  count(word, sentiment, sort = TRUE) %>%
+  ungroup()
+
+return(nyt_token_df)
+}
+
+nyt_bar_plot <- function(df, day, pos_or_neg)
+{
+  nyt_token_df <- df
+  nyt_ggtitle <- ggtitle(paste("Most used Words in NYT Charlottesville Coverage on August",
+                               day))
+  if(pos_or_neg == "Positive")
+  {
+    nyt_token_df <- filter(nyt_token_df, sentiment == "positive")
+    nyt_ggtitle <- ggtitle(paste("Most used", pos_or_neg, 
+                                     "Words in NYT Charlottesville Coverage on August",
+                                     day))
+  }
+  else if(pos_or_neg == "Negative")
+  {
+    nyt_token_df <- filter(nyt_token_df, sentiment == "negative")
+    nyt_ggtitle <- ggtitle(paste("Most used", pos_or_neg, 
+                                     "Words in NYT Charlottesville Coverage on August",
+                                     day))
+  }
+  
+  top_twenty_words_nyt <- head(nyt_token_df,20)
+  nyt_ggtheme <- theme(axis.text.x = element_text( size = 8, angle = 90))
+  
+  nyt_plot <- ggplot(data = top_twenty_words_nyt,aes(x = word, y = n)) +
+    geom_col(stat = "indentity")
+  
+  
+  return(nyt_plot + nyt_ggtheme + nyt_ggtitle)
+}
+  
+pct_graph_nyt<- function(df, day)
+{
+  nyt_token_df <- df
+  nyt_summary <- group_by(nyt_token_df, sentiment) %>%
+    summarise(
+      s_sum = sum(n)
+    )
+  total_words <- sum(nyt_summary$s_sum)
+  nyt_summary_pct <- mutate(nyt_summary,ratio=s_sum/sum(nyt_summary$s_sum))
+  p <- plot_ly(nyt_summary_pct, labels = ~sentiment, values = ~s_sum, type = 'pie') %>%
+    layout(title = paste("Percentage of Positive and Negative Words in the NYT Selections on August", day),
+           xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+           yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
+  return(p)
+}
 
