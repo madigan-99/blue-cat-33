35b499b524cccdf5ec05331ee97c658a25b8a18f
dr35-1623329 <35281404+dr35-1623329@users.noreply.github.com>
2019-06-04 18:12:52 -0700

    more changes

--- twitter_popularity.R
+++ twitter_popularity.R
@@ -5,4 +5,11 @@ library("tidyr")
 twitter_popularity <- function(data) {
   filter_data <- data %>% select(user_statuses_count, user_favorites_count, friends_count, followers_count)
   filter_data <- na.omit(filter_data)
+  sort_followers <- filter_data %>% group_by(followers_count) %>% summarise(avg_len = mean(length))
+  sort_statuses <- filter_data %>% group_by(user_statuses_count) %>% summarise(avg_len = mean(length))
+  sort_favorites <- filter_data %>% group_by(user_favorites_count) %>% summarise(avg_len = mean(length))
+  sort_friends <- filter_data %>% group_by(friends_count) %>% summarise(avg_len = mean(length))
+  
+#p <- plot_ly(data, )
+    
 }
