abab126aace6c4ca469e43ada1d37381f6b237e1
prettytotoro <13237158593@163.com>
2019-06-04 21:35:16 -0700

    fixed problem

--- Final_Project/server.R
+++ Final_Project/server.R
@@ -5,17 +5,14 @@ library(shinythemes)
 
 
 # loading related documents and library, read in dataset
-source("scripts/backer.R")
-source("scripts/chart#3.R")
-source("scripts/average goal.R")
-source("scripts/initial_info.R")
-source("scripts/total_backer.R")
-data <- read.csv("data/ks_projects_201801.csv", stringsAsFactors = F)
+source("../R/info project.R")
 
 # Start shinyServer
 shinyServer(function(input, output) {
 
-
+  output$`first-plot` <- renderPlotly({
+    first_graph(input$choose_date, input$data_type)
+  })
   
   
   
